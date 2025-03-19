eb11f197e3cfda8acf368140651f87a239b8dadd
nix-ones <beanixon@hotmail.fr>
2025-03-19 21:37:46 +0100

    debut spring

---
+++ src/main/java/com/example/task_management/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.task_management.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
